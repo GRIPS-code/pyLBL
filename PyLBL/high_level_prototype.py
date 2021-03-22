1bd33ea5abd20dddd0d91d724fbb3a184a877fe4
Raymond Menzel <Raymond.Menzel@noaa.gov>
2021-03-22 19:26:53 -0400

    backup, still needs to be tested

--- PyLBL/high_level_prototype.py
+++ PyLBL/high_level_prototype.py
@@ -1,7 +1,8 @@
 from os.path import isfile
 from sqlite3 import OperationalError
 
-from numpy import log, zeros
+from numpy import log, unravel_index, zeros
+from xarray import DataArray, Dataset
 
 from pyrad.lbl.continua import OzoneContinuum, WaterVaporForeignContinuum, \
                                WaterVaporSelfContinuum
@@ -16,81 +17,6 @@ air_molar_mass = 0.0289647  # Molar mass of air [kg mol-1].
 Na = 6.02214076e23  # Avogadro's number [mol-1].
 kb = 1.38064852e-23  # Boltzmann constant [J K-1].
 g = 9.80  # Acceleration due to gravity [m s-2].
-
-
-class Absorption(object):
-    """Container for absorption coefficients.
-
-    Attributes:
-        cia: Dictionary of arrays of absorption coefficients [m-1] from collision-induced
-             absorption tables.
-        continua: Array of absorption coefficients [m-1] from continua.
-        cross_section: Array of absorption coefficients [m-1] from cross section tables.
-        lines: Array of absorption coefficients [m-1] from spectral lines.
-    """
-    def __init__(self, lines, cross_section, cia, continua):
-        """Initializes object.
-
-        Args:
-            lines: Array of absorption coefficients [m-1] from spectral lines.
-            cross_section: Array of absorption coefficients [m-1] from cross section tables.
-            cia: Dictionary of arrays of absorption coefficients [m-1] from collision-induced
-                 absorption tables.
-            continua: Array of absorption coefficients [m-1] from continua.
-        """
-        self.lines = lines
-        self.cross_section = cross_section
-        self.cia = cia
-        self.continua = continua
-
-    @property
-    def total(self):
-        """Adds the absorption coefficient components together.
-
-        Returns:
-            Array of total absorption coefficients [m-1].
-        """
-        cia = zeros(self.lines.size)
-        for data in self.cia.values():
-            cia += data
-        return self.lines + self.cross_section + self.continua + cia
-
-
-class Atmosphere(object):
-    """Atmosphere object.
-
-    Attributes:
-        temperature: Array of temperatures [K].
-        pressure: Array of pressures [Pa].
-        volume_mixing_ratio: Dictionary where the keys are the molecule formulae and the
-                             values are arrays of volume-mixing ratios [mol mol-1].
-    """
-    def __init__(self, temperature, pressure, volume_mixing_ratio, z=None):
-        """Initializes object.
-
-        Args:
-            temperature: Array of temperatures [K].
-            pressure: Array of pressures [Pa].
-            volume_mixing_ratio: Dictionary where the keys are the molecule formulae and the
-                                 values are arrays of volume-mixing ratios [mol mol-1].
-        """
-        self.temperature = temperature
-        self.pressure = pressure
-        if z is None:
-            # Calculate distance between points assuming the atmosphere is 1D using the
-            # hydrostatic approximaxtion and ideal gas law.
-            self.z = zeros(temperature.size)
-            for i in range(1, self.z.size):
-               self.z[i] = self.z[i-1] + hydrostatic_distance(pressure[i-1:i+1],
-                                                              temperature[i])
-        else:
-            self.z = z
-        self.volume_mixing_ratio = volume_mixing_ratio
-
-    def points(self):
-        for i in range(self.temperature.size):
-            yield self.temperature[i], self.pressure[i], \
-                  {x: y[i] for x, y in self.volume_mixing_ratio.items()}
 
 
 class Gas(object):
@@ -114,7 +40,10 @@ class Gas(object):
             database: Path to database.
         """
         self.molecule = molecule
-        self.lines = Lines(molecule, hitran_database=database, tips_database=database)
+        try:
+            self.lines = Lines(molecule, hitran_database=database, tips_database=database)
+        except OperationalError:
+            pass
         try:
             self.cross_section = HitranCrossSection(molecule, database=database)
         except OperationalError:
@@ -125,13 +54,12 @@ class Gas(object):
                 self.cia[broadener] = HitranCIA(molecule, broadener, database=database)
             except OperationalError:
                 continue
+        self.continua = {}
         if molecule == "H2O":
-            self.continua = [x(database=database) for x  in [WaterVaporForeignContinuum,
-                                                             WaterVaporSelfContinuum]]
+            self.continua["foregin"] = WaterVaporForeignContinuum(database=database)
+            self.continua["self"] = WaterVaporSelfContinuum(database=database)
         elif molecule == "O3":
-            self.continua = [OzoneContinuum(database=database),]
-        else:
-            self.continua = []
+            self.continua["all"] = OzoneContinuum(database=database)
 
     def absorption_coefficient(self, temperature, pressure, volume_mixing_ratio, grid):
         """Calculates absorption coefficients.
@@ -144,29 +72,30 @@ class Gas(object):
             grid: Array of wavenumbers [cm-1].
 
         Returns:
-            Absorption object.
+            Dictionary of absorption coefficients.
         """
         vmr = volume_mixing_ratio[self.molecule]
         n = number_density(pressure, temperature, vmr)
-        k_lines = self.lines.absorption_coefficient(temperature, pressure, vmr, grid)*n
+        k = {}
+        k["lines"] = self.lines.absorption_coefficient(temperature, pressure, vmr, grid)*n
         try:
-            k_cross_section = self.cross_section.absorption_coefficient(temperature, pressure,
-                                                                        grid)*n
+            k["cross section"] = self.cross_section.absorption_coefficient(temperature, pressure,
+                                                                           grid)*n
         except AttributeError:
-            k_cross_section = zeros(grid.size)
-        k_cia = {x: zeros(grid.size) for x in self.cia.keys()}
+            k["cross section"] = zeros(grid.size)
+        k["cia"] = {x: zeros(grid.size) for x in self.cia.keys()}
         for broadener, cia in self.cia.items():
-            k_cia[broadener] += cia.absorption_coefficient(temperature, grid)*n * \
-                                number_density(pressure, temperature,
-                                volume_mixing_ratio[broadener])
-        k_continua = zeros(grid.size)
-        for continuum in self.continua:
+            k["cia"][broadener] += cia.absorption_coefficient(temperature, grid)*n * \
+                                   number_density(pressure, temperature,
+                                                  volume_mixing_ratio[broadener])
+        k["continua"] = {x: zeros(grid.size) for x in self.continua.keys()}
+        for mechanism, continuum in self.continua.items():
             try:
-                k_continua += continuum.absorption_coefficient(temperature, pressure,
-                                                               vmr, grid)*n
+                k["continua"][mechanism] += continuum.absorption_coefficient(temperature, pressure,
+                                                                             vmr, grid)*n
             except TypeError:
-                k_continua += continuum.absorption_coefficient(grid)*n
-        return Absorption(k_lines, k_cross_section, k_cia, k_continua)
+                k["continua"][mechanism] += continuum.absorption_coefficient(grid)*n
+        return k
 
 
 class Spectroscopy(object):
@@ -227,33 +156,43 @@ class Spectroscopy(object):
            pressure, and gas concentrations 
 
         Args:
-            atmosphere: an Atmosphere object.
+            atmosphere: an xarray Dataset.
             grid: Wavenumber grid array [cm-1].
 
         Returns:
             An array of absorption coefficients.
         """
-        k = {x: zeros((atmosphere.pressure.size, grid.size)) for x in self.gas.keys()}
+        p = atmosphere["pressure"]
+        coords = {key: value for key, value in p.coords.items()}
+        coords["mechanism"] = ["lines", "cross_section", "cia", "continua"]
+        coords["wavenumber"] = grid
+        dims = list(p.dims) + ["mechanism", "wavenumber"]
+        sizes = tuple([len(coords[x]) for x in dims])
+        beta = {"{}_absorption".format(name): DataArray(zeros(sizes), coords=coords, dims=dims)
+                for name in self.gas.keys()}
         for name, gas in self.gas.items():
-            for i, (temperature, pressure, vmr) in enumerate(atmosphere.points()): 
-                k[name][i, :] = gas.absorption_coefficient(temperature, pressure, vmr,
-                                                           grid).total
-        return k
-
-
-def hydrostatic_distance(pressure, temperature, molar_mass=air_molar_mass):
-    """Calculates the distance between two pressures assuming hydrostatic equilibrium
-       and constant temperature, using the ideal gas law.
-
-    Args:
-        pressure: 2-element array of pressures [Pa].
-        temperature: Temperature [K].
-        molar_mass: Molar mass [kg mol-1].
-
-    Returns:
-        Distance [m] between the two atmospheric pressures.
-    """
-    return (Na*kb*temperature/(molar_mass*g))*log(pressure[1]/pressure[0])
+            for i in range(atmosphere["pressure"].data.size):
+                vmr = {x: atmosphere["vmr_{}".format(x)].data.flat[i]
+                       for x in self.gas.keys()}
+                k = gas.absorption_coefficient(atmosphere["temperature"].data.flat[i],
+                                               p.data.flat[i], vmr, grid)
+                i = unravel_index(i, p.data.shape)
+                for j, (source, data) in enumerate(k.items()):
+                    indices = tuple(list(i) + [j, slice(None)])
+                    print(indices)
+                    if source == "cia":
+                        bsum = zeros(grid.size)
+                        for values in data.values():
+                            bsum += values[:]
+                        beta["{}_absorption".format(name)].values[indices] = bsum[:]
+                    elif source == "continua":
+                        bsum = zeros(grid.size)
+                        for values in data.values():
+                            bsum += values[:]
+                        beta["{}_absorption".format(name)].values[indices] = bsum[:]
+                    else:
+                        beta["{}_absorption".format(name)].values[indices] = data[:]
+        return Dataset(beta)
 
 
 def number_density(pressure, temperature, volume_mixing_ratio):
