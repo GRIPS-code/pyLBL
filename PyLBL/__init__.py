93fec4a7d859789a181812d26b7d0b9207e5d2b1
Raymond Menzel <Raymond.Menzel@noaa.gov>
2021-03-22 19:26:53 -0400

    backup, still needs to be tested

--- PyLBL/__init__.py
+++ PyLBL/__init__.py
@@ -1,3 +1,3 @@
-from .high_level_prototype import Atmosphere, Spectroscopy
+from .high_level_prototype import Spectroscopy
 from .low_level_prototype import collision_induced_absorption, continua, cross_sections, \
                                  models, molecular_lines
