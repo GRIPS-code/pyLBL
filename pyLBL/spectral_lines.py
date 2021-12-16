add documentation and fix units

pyLBL/spectral_lines.py
@@ -7,8 +7,12 @@ from scipy.special import wofz
 from .tips import TIPS_REFERENCE_TEMPERATURE, TotalPartitionFunction
 
 
+avogadro = 6.0221409e23 # Avogadro's constant [mol-1].
+boltzmann = 1.380658e-16  # Boltzmann constant [erg K-1].
+cm_to_m = 0.01  # [m cm-1].
 c2 = -1.4387768795689562  # (hc/k) [K cm].
-parameters = ["d_air", "en", "gamma_air", "gamma_self", "iso", "n_air", "s", "v"]
+light_speed = 2.99792458e10  # Speed of light [cm s-1].
+pa_to_atm = 9.86923e-6  # [Pa-1 atm].
 reference_temperature = 296.  # [K].
 sqrt_log2 = sqrt(log(2))
 sqrt_2 = sqrt(2)
@@ -17,8 +21,26 @@ sqrt_2pi = sqrt(2*pi)
 
 
 class SpectralLines(object):
+    """HITRAN spectral line parameters.
+
+    Attributes:
+        d_air: Transition pressure shift factor.
+        en: Lower state energy level.
+        gamma_air: Air-broadened halfwidth.
+        gamma_self: Self-broadened halfwidth.
+        iso: Isotopologue id.
+        n_air: Air-broadening temperature dependence power.
+        s: Transition strength.
+        v: Transition wavenumber.
+    """
+    parameters = ["d_air", "en", "gamma_air", "gamma_self", "iso", "n_air", "s", "v"]
     def __init__(self, transitions):
-        for x in parameters:
+        """Converts from HAPI transitions objects to numpy arrays.
+
+        Args:
+            transitions: List of Transition objects
+        """
+        for x in self.parameters:
             setattr(self, x, [])
         for transition in transitions:
             self.d_air.append(transition.delta_air)
@@ -29,15 +51,42 @@ class SpectralLines(object):
             self.n_air.append(transition.n_air)
             self.s.append(transition.sw)
             self.v.append(transition.nu)
-        for x in parameters:
+        for x in self.parameters:
             setattr(self, x, asarray(getattr(self, x)))
 
 
 def initial_strength_correction(s, q, t, iso, en, v):
+    """Initial correction for the line strengths.
+
+    Args:
+        s: Line strength [cm].
+        q: TotalPartitionFunction object.
+        t: Temperature [K].
+        iso: HITRAN isotopologue id.
+        en: Transition lower state energy [cm-1].
+        v: Transition wavenumber [cm-1].
+
+    Returns:
+        Corrected line strengths [cm].
+    """
     return s*temperature_correct_line_strength(q, t, iso, en, v)
 
 
 def brute_force(wavenumber, center, strength, gamma, alpha, remove_pedestal, cut_off=25.):
+    """Calculates absorption coefficients [cm2].
+
+    Args:
+        wavenumber: Numpy array of wavenumbers [cm-1].
+        center: Numpy array of transition wavenumbers [cm-1].
+        strength: Numpy array of transition strengths [cm].
+        gamma: Numpy array of pressure-broadened halfwidths [cm-1].
+        alpha: Numpy array of doppler-broadened halfwidths [cm-1].
+        remove_pedestal: Flag for removing the MT-CKD continnum pedestal.
+        cut_off: Spectral cut-off from line center [cm-1].
+
+    Returns:
+        Numpy array of absorption coefficients [cm2].
+    """
     left = searchsorted(wavenumber, center - cut_off, side="left")
     right = searchsorted(wavenumber, center + cut_off, side="right")
     k = zeros(wavenumber.size)
@@ -51,16 +100,48 @@ def brute_force(wavenumber, center, strength, gamma, alpha, remove_pedestal, cut
 
 def line_profile(v, s, q, iso, en, d_air, n_air, n_self, gamma_air, gamma_self,
                  mass, t, p, vmr, wavenumber, cut_off=25., remove_pedestal=False):
+    """Calculates absorption coefficients [m2].
+
+    Args:
+        v: Numpy array of transition wavenumbers [cm-1].
+        s: Numpy array of transition strengths [cm].
+        q: TotalPartitionFunction object.
+        iso: Numpy array of HITRAN isotopologue ids.
+        en: Numpy array of transition lower state energy levels [cm-1].
+        d_air: Numpy array of air-broadened transition pressure shifts [atm-1 cm-1].
+        n_air: Numpy array of air-broadened temperature dependence powers.
+        n_self: Numpy array of self-broadened temperature dependence powers.
+        gamma_air: Numpy array of air-broadened halfwidths [cm-1 atm-1].
+        gamma_self: Numpy array of self-broadened halfwidths [cm-1 atm-1].
+        mass: Molecular mass [g mol-1].
+        t: Temperature [K].
+        p: Pressure [atm].
+        vmr: Volume mixing ratio [mol mol-1].
+        wavenumber: Numpy array of wavenumbers [cm-1].
+        cut_off: Spectral cut-off from line center [cm-1].
+        remove_pedestal: Flag for removing the MT-CKD continnum pedestal.
+
+    Returns:
+        Numpy array of absorption coefficients [m2].
+    """
     center = pressure_shift_transition_wavenumber(v, d_air, p)
     strength = s / temperature_correct_line_strength(q, t, iso, en, v)
     gamma = pressure_broadened_halfwidth(p, p*vmr, t, n_air, n_self, gamma_air, gamma_self)
     alpha = doppler_broadened_halfwidth(t, mass[iso - 1], v)
     k = brute_force(wavenumber, center, strength, gamma, alpha, remove_pedestal, cut_off)
-    return k
+    return k*cm_to_m*cm_to_m
 
 
 class Gas(object):
     def __init__(self, transitions, formula, molecule_id, isotopologues):
+        """Initializes object.
+
+        Args:
+            transitions: List of HAPI Transitions objects.
+            formula: String chemical formula.
+            molecule_id: Integer Hitran molecule identifier.
+            isotopologues: List of HAPIT Isotopologue objects.
+        """
         self.mass = asarray([float(x.mass) for x in isotopologues])
         self.transitions = SpectralLines(transitions)
         self.q = TotalPartitionFunction(formula)
@@ -70,12 +151,23 @@ class Gas(object):
 
     def absorption_coefficient(self, temperature, pressure, volume_mixing_ratio, grid,
                                continuum="mt-ckd"):
+        """Calculates absorption coefficients for the gas.
+
+        Args:
+            temperature: Temperature [K].
+            pressure: Pressure [Pa].
+            volume_mixing_ratio: Volume mixing ratio [mol mol-1].
+            grid: Wavenumber grid [cm-1].
+
+        Returns:
+            Absorption coefficients [m2].
+        """
         return line_profile(self.transitions.v, self.s, self.q, self.transitions.iso,
                             self.transitions.en, self.transitions.d_air, 
                             self.transitions.n_air, self.transitions.n_air,
                             self.transitions.gamma_air, self.transitions.gamma_self,
-                            self.mass, temperature, pressure, volume_mixing_ratio,
-                            grid, remove_pedestal=continuum == "mt-cdk")
+                            self.mass, temperature, pressure*pa_to_atm,
+                            volume_mixing_ratio, grid, remove_pedestal=continuum == "mt-cdk")
 
 
 def pressure_shift_transition_wavenumber(line_center, delta_air, pressure):
@@ -118,8 +210,8 @@ def voigt_profile(dv, pressure_halfwidth, doppler_halfwidth):
 
     Args:
         dv: Wavenumber distance from line center [cm-1].
-        pressure_halfwidth: Pressure-broadened line half-width [cm -1].
-        doppler_halfwidth: Doppler-broadened line half-width [cm -1].
+        pressure_halfwidth: Pressure-broadened line half-width [cm-1].
+        doppler_halfwidth: Doppler-broadened line half-width [cm-1].
 
     Returns:
         Voigt line profile broadening [cm].
@@ -135,9 +227,9 @@ def pressure_broadened_halfwidth(pressure, partial_pressure, temperature,
     Args:
         pressure: Pressure [atm].
         partial_pressure: Partial pressure [atm].
-        temperature: Temperature [K]
-        n_air: Air-broadened temperature dependence powers.
-        n_self: Self-broadened temperature dependence powers.
+        temperature: Temperature [K].
+        n_air: Air-broadened temperature dependence power.
+        n_self: Self-broadened temperature dependence power.
         gamma_air: Air-broadened halfwidth [cm-1 atm-1].
         gamma_self: Self-broadened halfwidth [cm-1 atm-1].
 
@@ -159,7 +251,6 @@ def doppler_broadened_halfwidth(temperature, mass, transition_wavenumber):
     Returns:
         Doppler-broadened line halfwidth [cm-1].
     """
-    m = mass/6.023e23  # Mass/Avagadro's number [g].
-    c = 2.99792458e10  # Speed of light [cm s-1].
-    kb = 1.380658e-16  # Boltzmann constant [erg K-1].
-    return sqrt_log2*transition_wavenumber*sqrt(2.*kb*temperature/(m*c*c))
+    m = mass/avogadro  # Mass/Avogadro's number [g].
+    return sqrt_log2*transition_wavenumber*sqrt(2.*boltzmann*temperature/ \
+        (m*light_speed*light_speed))